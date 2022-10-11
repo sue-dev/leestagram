ab39f05ddeb001e3043b926e86c8e1a9e6adc930
sue-dev <java.suhyun@gmail.com>
2022-10-11 19:36:47 +0900

    구글 로그인 성공

--- app/src/main/java/com/study/leestagram/LoginActivity.kt
+++ app/src/main/java/com/study/leestagram/LoginActivity.kt
@@ -1,17 +1,25 @@
 package com.study.leestagram
 
+import android.R.attr.data
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
@@ -24,8 +32,6 @@ class LoginActivity : AppCompatActivity() {
     val password_edittext: EditText by lazy {
         findViewById(R.id.password_edittext)
     }
-
-    lateinit var registerForActivityResult: ActivityResultLauncher<Intent>
 
     private val email_login_button: AppCompatButton by lazy {
         findViewById(R.id.email_login_button)
@@ -44,22 +50,45 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
-        google_sign_in_button.setOnClickListener {
-            googleSignin()
-        }
 
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail().build()
-        googleSigninClient = GoogleSignIn.getClient(this, gso)
+            .requestEmail()
+            .requestIdToken("1071232915052-avpenq3a3r9f6ej1ir376dhan6394b4l.apps.googleusercontent.com").build()
 
+        googleSigninClient = GoogleSignIn.getClient(this, gso)
+        var googleSignInIntent: Intent? = googleSigninClient?.getSignInIntent()
+        var resultLauncher: ActivityResultLauncher<Intent> =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result:ActivityResult ->
+                Log.d("RESULT", result.toString())
+                if(result.resultCode == RESULT_OK){
+                    val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    val account = task.getResult()
+                    Log.d("RESULT DATA: account", account.toString())
+                    Log.d("RESULT DATA: task ", task.toString())
+
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        google_sign_in_button.setOnClickListener {
+            resultLauncher.launch(googleSignInIntent)
+        }
     }
 
-    private fun googleSignin() {
-        var signInIntent = googleSigninClient?.getSignInIntent()
-
-//        TODO:: intent 호출해서 로그인화면으로 이동
-       registerForActivityResult = registerForActivityResult()
-//        (signInIntent, GOOGLE_LOGIN_CODE)
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.getIdToken(), null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // create a user account
+                    moveMainPage(task.result?.user)
+                } else if (!task.exception?.message.isNullOrEmpty()) {
+                    // show the error message
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    // login if you have account
+                    signinEmail()
+                }
+            }
     }
 
     fun signinAndSignup() {
@@ -82,6 +111,9 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun moveMainPage(user: FirebaseUser?) {
+        if (user != null){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
 
     }
 
