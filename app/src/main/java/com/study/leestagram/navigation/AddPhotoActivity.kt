1f346bd903f00691e76eac48a19babc9cefb3967
sue-dev <java.suhyun@gmail.com>
2022-10-14 01:28:27 +0900

    ui작업

---
+++ app/src/main/java/com/study/leestagram/navigation/AddPhotoActivity.kt
@@ -0,0 +1,12 @@
+package com.study.leestagram.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.study.leestagram.R
+
+class AddPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+    }
+}
