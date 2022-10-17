2e5202c49a9a8034bd3e436c3565f408c653896e
sue-dev <java.suhyun@gmail.com>
2022-10-17 15:32:42 +0900

    firestore db write

--- app/src/main/java/com/study/leestagram/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/study/leestagram/navigation/AddPhotoActivity.kt
@@ -1,5 +1,6 @@
 package com.study.leestagram.navigation
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
@@ -7,13 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.study.leestagram.R
+import com.study.leestagram.navigation.model.ContentDTO
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 
@@ -21,9 +26,15 @@ class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_ALBUM = 0
     var storage: FirebaseStorage? = null
     var photoUri: Uri? = null
+    var auth :FirebaseAuth? = null
+    var firestore :FirebaseFirestore? = null
 
     val add_photo_image : ImageView by lazy {
         findViewById(R.id.add_photo_image)
+    }
+
+    val add_photo_edit_description : EditText by lazy {
+        findViewById(R.id.add_photo_edit_description)
     }
 
     val add_photo_btn_upload : Button by lazy {
@@ -34,13 +45,10 @@ class AddPhotoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
-        // TODO:: FIREBASE STORAGE 이용 이미지 업로드 기능
-        // 1. 앨범에서 사진 가져오기
-        // 2. firebase storage에 사진 업로드하기
-
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
-        // 1. 앨범
         var intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
 
@@ -56,15 +64,26 @@ class AddPhotoActivity : AppCompatActivity() {
 
     }
 
-    //2. 사진 업로드
     private fun contentUpload() {
         val timestamp: String = System.currentTimeMillis().toString()
         var storagePath = storage?.reference?.child("images")?.child(timestamp)
         storagePath?.putFile(photoUri!!)
             ?.addOnCompleteListener{ task->
                 if(task.isSuccessful){
-                    val downloadUrl = task.result
-                    Toast.makeText(this, "업로드가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                    storagePath.downloadUrl.addOnSuccessListener { uri->
+                        var contentDTO = ContentDTO()
+                        contentDTO.imageUrl = uri.toString()
+                        contentDTO.uid = auth?.currentUser?.uid
+                        contentDTO.userId = auth?.currentUser?.email
+                        contentDTO.description = add_photo_edit_description.text.toString()
+                        contentDTO.timestamp = System.currentTimeMillis()
+
+                        firestore?.collection("images")?.document()?.set(contentDTO)
+
+                        Toast.makeText(this, "업로드가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                        setResult(Activity.RESULT_OK)
+                        finish()
+                    }
                 }
             }
     }
---
+++ app/src/main/java/com/study/leestagram/navigation/model/ContentDTO.kt
@@ -0,0 +1,18 @@
+package com.study.leestagram.navigation.model
+
+data class ContentDTO(
+    var description : String? = null,
+    var imageUrl : String? = null,
+    var uid : String? = null,
+    var userId : String? = null,
+    var timestamp : Long? = null,
+    var favoriteCount : Int = 0,
+    var favorites : Map<String, Boolean> = HashMap()
+                        ){
+    data class Comment(
+        var uid : String? = null,
+        var userId : String? = null,
+        var comment : String? = null,
+        var timestamp: Long? = null
+    )
+}
