75dfaf53251aa18c86f44c4ae1e3d2eae287709a
sue-dev <java.suhyun@gmail.com>
2022-11-10 13:08:21 +0900

    :sparkles: user feed

--- app/src/main/java/com/study/leestagram/navigation/UserFragment.kt
+++ app/src/main/java/com/study/leestagram/navigation/UserFragment.kt
@@ -5,15 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.study.leestagram.BaseFragment
 import com.study.leestagram.R
+import com.study.leestagram.databinding.FragmentUserBinding
 
-class UserFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
-        return view
+class UserFragment : BaseFragment<FragmentUserBinding>(FragmentUserBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
+
+    inner class UserFragmentRecyclerAdaptor: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        init {
+            firestore?.collection("contents").
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+
+
 }
